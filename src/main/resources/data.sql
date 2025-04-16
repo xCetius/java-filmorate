4adbaf95495746e54941f0f103d3b335c9438ffa
Cetius <heajler@gmail.com>
2025-04-16 14:38:11 +0300

    Исправить data.sql для тестов постман

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,10 @@
+DELETE FROM ratings;
 INSERT INTO ratings (RATING_ID, name) VALUES (1,'G');
 INSERT INTO ratings (RATING_ID, name) VALUES (2,'PG');
 INSERT INTO ratings (RATING_ID, name) VALUES (3,'PG-13');
 INSERT INTO ratings (RATING_ID, name) VALUES (4,'R');
 INSERT INTO ratings (RATING_ID, name) VALUES (5,'NC-17');
-
+DELETE FROM genres;
 INSERT INTO genres (GENRE_ID,name) VALUES (1,'Комедия');
 INSERT INTO genres (GENRE_ID,name) VALUES (2,'Драма');
 INSERT INTO genres (GENRE_ID,name) VALUES (3,'Мультфильм');
